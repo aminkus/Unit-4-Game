1f7c7ec67e11f8b440e8fee8c06cb5fcda84dc7c
Adam Minkus <adam.minkus@gmail..com>
2019-06-03 20:13:13 -0700

    working version of game. Needs style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,110 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-//Generate random number for target score
-var getRandomNumber = Math.floor(Math.random() * (120-19 + 1)) + 19;
+    //Generate random number
+    var getRandomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+   
 
-//Display randomized number to HTML
-$("#scoreToMatch").text(getRandomNumber);
+    //Display randomized number to HTML
+    $("#scoreToMatch-text").text(getRandomNumber);
 
-//Generate random number for each crystal
-var num1 = Math.floor(Math.random() * 12+1);
-var num2 = Math.floor(Math.random() * 12+1);
-var num3 = Math.floor(Math.random() * 12+1);
-var num4 = Math.floor(Math.random() * 12+1);
+    //Generate random number for each crystal
+    var num1 = Math.floor(Math.random() * 12 + 1);
+    var num2 = Math.floor(Math.random() * 12 + 1);
+    var num3 = Math.floor(Math.random() * 12 + 1);
+    var num4 = Math.floor(Math.random() * 12 + 1);
 
-//create variable to track wins, losses, players total
-var wins = 0;
-var losses = 0;
-var playerTotal = 0;
 
-//Display wins, losses, and player total
-$("#wins-text").text(wins);
-$("#losses-text").text(losses);
-$("#totalScore-text").text(playerTotal);
+    //create variable to track wins, losses, players total
+    var wins = 0;
+    var losses = 0;
+    var playerTotal = 0;
 
-//Build function to reset game
-function reset () {
-    getRandomNumber = Math.floor(Math.random() * (120-19 + 1)) + 19;
-    $("#scoreToMatch").text(getRandomNumber);
-    var num1 = Math.floor(Math.random() * 12+1);
-    var num2 = Math.floor(Math.random() * 12+1);
-    var num3 = Math.floor(Math.random() * 12+1);
-    var num4 = Math.floor(Math.random() * 12+1);
-    playerTotal = 0;
-    $("#totalScore").text(playerTotal);
-}
-
-//Build function when there is a winner, increases counter and displays to HTML
-
-function playerWins () {
-    alert ("Good Job! You Won!");
-    wins++;
+    //Display wins, losses, and player total
     $("#wins-text").text(wins);
-    reset();
-}
-
-//Build function when there is a loser, increases counter and displays to HTML
-
-function playerLoses () {
-    alert("You Lose. Try Again");
-    losses++;
     $("#losses-text").text(losses);
-    reset ();
-}
+    $("#totalScore-text").text(playerTotal);
 
-//Set up event listener for cystals and add random number to playerTotal
-$(".red").on("click", function () {
-    playerTotal = playerTotal + num1;
-    $("#totalScore").text(playerTotal);
-})
+    //Build function to reset game
+    function reset() {
+        getRandomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+        $("#scoreToMatch-text").text(getRandomNumber);
+        var num1 = Math.floor(Math.random() * 12 + 1);
+        var num2 = Math.floor(Math.random() * 12 + 1);
+        var num3 = Math.floor(Math.random() * 12 + 1);
+        var num4 = Math.floor(Math.random() * 12 + 1);
+        playerTotal = 0;
+        $("#totalScore-text").text(playerTotal);
+    }
+
+    //Build function when there is a winner, increases counter and displays to HTML
+
+    function playerWins() {
+        alert("Good Job! You Won!");
+        wins++;
+        $("#wins-text").text(wins);
+        reset();
+    }
+
+    //Build function when there is a loser, increases counter and displays to HTML
+
+    function playerLoses() {
+        alert("You Lose. Try Again");
+        losses++;
+        $("#losses-text").text(losses);
+        reset();
+    }
+
+    //Set up event listener for cystals and add random number to playerTotal
+    $(".red").on("click", function () {
+        playerTotal = playerTotal + num1;
+        $("#totalScore-text").text(playerTotal);
+        if (playerTotal === getRandomNumber) {
+            playerWins();
+        }
+        else if (playerTotal > getRandomNumber) {
+            playerLoses();
+        }
+    })
+
+    $(".blue").on("click", function () {
+        playerTotal = playerTotal + num2;
+        $("#totalScore-text").text(playerTotal);
+        if (playerTotal === getRandomNumber) {
+            playerWins();
+        }
+        else if (playerTotal > getRandomNumber) {
+            playerLoses();
+        }
+    })
+
+    $(".green").on("click", function () {
+        playerTotal = playerTotal + num3;
+        $("#totalScore-text").text(playerTotal);
+        if (playerTotal === getRandomNumber) {
+            playerWins();
+        }
+        else if (playerTotal > getRandomNumber) {
+            playerLoses();
+        }
+
+    })
+
+    $(".yellow").on("click", function () {
+        playerTotal = playerTotal + num4;
+        $("#totalScore-text").text(playerTotal);
+        if (playerTotal === getRandomNumber) {
+            playerWins();
+        }
+        else if (playerTotal > getRandomNumber) {
+            playerLoses();
+        }
+
+    })
+
+   
+
+
+
 
 
 
